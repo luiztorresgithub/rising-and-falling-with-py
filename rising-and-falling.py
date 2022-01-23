d58c149b170ac16321aa8f21ea70bef23e0d7028
Luiz Felipe Torres Santos <94366321+luiztorresgithub@users.noreply.github.com>
2022-01-23 14:08:45 -0300

    Initial commit

---
+++ rising-and-falling.py
@@ -0,0 +1,10 @@
+x = int(input('digite o seu número inteiro'))
+y = int(input('digite o seu número inteiro'))
+z = int(input('digite o seu número inteiro'))
+
+if z > y and y > x:
+    print ('crescente')
+else:
+    print('não está em ordem crescente')
+
+        
